fcc5390bce9b043588ce12b3fbf37ded05cd186c
Laurent Picouleau <l.picouleau@gmail.com>
2017-11-14 12:56:36 +0100

    slightly faster num_util:log2_est/1

--- src/num_util.erl
+++ src/num_util.erl
@@ -1,18 +1,14 @@
 -module(num_util).
 
--define(N31, 2147483648). % 1 bsl 31
+-define(N32, 4294967296). % 1 bsl 32
 
 -export([log2_est/1, p2/1]).
 
 log2_est(N) when N < 256 -> log2_bin(N);
-log2_est(N) when N < 65536 -> 8 + log2_bin(N bsr 8);
-log2_est(N) when N < 16777216 -> 16 + log2_bin(N bsr 16);
-log2_est(N) when N < 4294967296 -> 24 + log2_bin(N bsr 24);
 log2_est(N) ->
-	case term_to_binary(N) of
-		<<131, 111, S:32, R/binary>> -> 8 * S - 8 + log2_bin(binary:last(R));
-		<<131, 110, S, R/binary>> -> 8 * S - 8 + log2_bin(binary:last(R))
-	end.
+	BN = binary:encode_unsigned(N),
+	<<H:8, _/binary>> = BN,
+	byte_size(BN) bsl 3 - 8 + log2_bin(H).
 
 log2_bin(N) when N > 15 -> log2_bin_l(N);
 log2_bin(N) -> log2_bin_s(N).
@@ -20,8 +16,8 @@ log2_bin(N) -> log2_bin_s(N).
 log2_bin_l(N) when N > 63 -> (N band 128) bsr 7 + 7;
 log2_bin_l(N) -> (N band 32) bsr 5 + 5.
 
-log2_bin_s(0) -> 0;
 log2_bin_s(N) when N > 3 -> (N band 8) bsr 3 + 3;
+log2_bin_s(0) -> 0;
 log2_bin_s(N) -> (N band 2) bsr 1 + 1.
 
 % the largest power of 2 dividing N
