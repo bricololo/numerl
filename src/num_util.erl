e6be1ded350fba175afac56e8c2ea929a9cd3f66
Laurent Picouleau <l.picouleau@gmail.com>
2017-11-16 22:10:14 +0100

    faster p2/1

--- src/num_util.erl
+++ src/num_util.erl
@@ -1,6 +1,6 @@
 -module(num_util).
 
--define(N32, 4294967296). % 1 bsl 32
+-define(N64, 18446744073709551616). % 1 bsl 64
 
 -export([bytes/1, log2_est/1, p2/1, hamming/1]).
 
@@ -27,35 +27,33 @@ log2_bin_s(N) -> (N band 2) bsr 1 + 1.
 % the largest power of 2 dividing N
 p2(0) -> 0;
 p2(N) when N band 1 =:= 1 -> 0;
-p2(N) when N < ?N32 -> p2(N, 0);
+p2(N) when N < ?N64 -> p2_64(N);
 p2(N) ->
-	case term_to_binary(N) of
-		<<131, 110, _:16, Bin/binary>> -> p2bin(0, Bin);
-		<<131, 111, _:40, Bin/binary>> -> p2bin(0, Bin)
+	case N band 16#ffffffffffffffff of
+		0 ->
+			case term_to_binary(N) of
+				<<131, 110, _:80, Bin/binary>> -> p2_bin(64, Bin);
+				<<131, 111, _:104, Bin/binary>> -> p2_bin(64, Bin)
+			end;
+		V -> p2_64(V)
 	end.
 
-p2_8(N) ->
-	case N bxor (N - 1) of
-		1 -> 0;
-		3 -> 1;
-		7 -> 2;
-		15 -> 3;
-		31 -> 4;
-		63 -> 5;
-		127 -> 6;
-		255 -> 7
-	end.
+p2_64(N) ->
+	lists:nth(
+		(N bxor (N - 1)) rem 67, % the remainder can't be 0, 33 or 66
+		[0,38,1,14,39,22,2,11,15,58,40,18,23,53,3,63,12,9,16,61,59,27,41,29,19,
+		50,24,43,54,46,4,31,bad,37,13,21,10,57,17,52,62,8,60,26,28,49,42,45,30,
+		36,20,56,51,7,25,48,44,35,55,6,47,34,5,33,32]).
 
-p2(N, P) when N < 256 -> P + p2_8(N);
-p2(N, P) ->
-	case N band 255 of
-		0 -> p2(N bsr 8, P + 8);
-		V -> P + p2_8(V)
-	end.
+p2_bin(P, <<0:64, Bin/binary>>) -> p2_bin(P + 64, Bin);
+p2_bin(P, <<T:8/binary, _/binary>>) -> p2_f(P, T);
+p2_bin(P, Bin) -> p2_f(P, Bin).
 
-p2bin(P, <<0:32, Bin/binary>>) -> p2bin(P + 32, Bin);
-p2bin(P, <<0:8, Bin/binary>>) -> p2bin(P + 8, Bin);
-p2bin(P, <<V:8, _/binary>>) -> p2(V, P).
+p2_f(P, T) ->
+	P +
+		p2_64(
+			binary:decode_unsigned(
+				list_to_binary(lists:reverse(binary_to_list(T))))).
 
 % count the numbers of bits set to 1 in N.
 hamming(N) when N < 268435456 -> hamming28(N);
