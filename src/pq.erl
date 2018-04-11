e203392b34dcc55c0506477013bec924e5f6f99f
Laurent Picouleau <l.picouleau@gmail.com>
2018-04-11 16:25:08 +0200

    clearer organization of the code

--- src/pq.erl
+++ src/pq.erl
@@ -4,52 +4,58 @@
 
 new(Cur, Next) -> {Cur, Next, empty}.
 
-leaf(P) -> {P, nil, nil}.
+val({_, _, empty}) -> nothing;
+val({Cur, _, _, {P, _, _}}) -> Cur(P).
+
+% add a new element to the priority queue. The priority queue is managed as a
+% heap, so:
+% Creating the root of the heap
+add({Cur, Next, empty}, Elem) -> {Cur, Next, 1, leaf(Elem)};
+% adding the Sz + 1 element to the heap T at the position given by path/1
+add({Cur, Next, Size, Heap}, Elem) ->
+	{Cur, Next, Size + 1, add(Heap, Elem, path(Size + 1))}.
+
+% remove the composite N from Heap of Size, the size of the heap will not change
+% as a composite removed is replaced by a bigger composite.
+bump({Cur, Next, Size, Heap}, N) ->
+	{Cur, Next, Size, bumpt(Heap, N, Cur, Next)}.
+
+%%
+%% implementation
+%%
+
+leaf(Elem) -> {Elem, nil, nil}.
 
 path(N) -> path(N, []).
 
 path(1, L) -> L;
 path(N, L) -> path(N bsr 1, [N band 1 | L]).
 
-% add a new element to the priority queue. The priority queue is managed as a
-% heap, so:
-% Creating the root of the heap
-add({Cur, Next, empty}, Elem) -> {Cur, Next, 1, leaf(Elem)};
-% adding the Sz + 1 element to the tree T at the position given by path/1
-add({Cur,Next,Size,T}, Elem) -> {Cur, Next, Size+1, add(T,Elem,path(Size+1))}.
-
-% adding an element by creating an empty subtree as a leaf
 add(nil, Elem, []) -> leaf(Elem);
-% going through tre Left (resp. Right) subtree until reaching the missing leaf
-add({P, L, R}, Elem, [0 | Path]) -> {P, add(L, Elem, Path), R};
-add({P, L, R}, Elem, [1 | Path]) -> {P, L, add(R, Elem, Path)}.
+add({Head, Left, Right}, Elem, [0 | Path]) ->
+	{Head, add(Left, Elem, Path), Right};
+add({Head, Left, Right}, Elem, [1 | Path]) ->
+	{Head, Left, add(Right, Elem, Path)}.
 
-val({_, _, empty}) -> nothing;
-val({Cur, _, _, {P, _, _}}) -> Cur(P).
-
-% remove the composite N from the tree T of size Sz, the size of the tree will
-% not change as a composite removed is replaced by a bigger composite.
-bump({Cur, Next, Sz, T}, N) -> {Cur, Next, Sz, bumpt(T, N, Cur, Next)}.
-
-% a composite might be present several times (for example 2431 will be present
-% 3 times, as it is multiple of 11, 13 and 17). So we remove V from T until the
-% smallest composite in T is larger than V.
-bumpt({P, L, R} = T, V, Cur, Next) ->
-	case Cur(P) of
-		Vp when Vp =< V -> bumpt(fix({Next(P), L, R}, Cur), V, Cur, Next);
-		_ -> T
+% an element might be present several times So we replace V by its next value
+% until the smallest element in T is larger than V.
+bumpt({Head, Left, Right} = Heap, V, Cur, Next) ->
+	case Cur(Head) of
+		Vp when Vp =< V ->
+			bumpt(fix({Next(Head), Left, Right}, Cur), V, Cur, Next);
+		_ -> Heap
 	end.
 
-% adjust heap by swapping primes down until it's a heap again
+% adjust heap by swapping elements down until it's a heap again
 fix({_, nil, nil} = T, _) -> T;
-fix({P, {Pl, nil, nil}, nil} = T, Cur) ->
-	case {Cur(P), Cur(Pl)} of
-		{V, Vl} when V < Vl -> T;
-		_ -> {Pl, {P, nil, nil}, nil}
+fix({Head, {Left, nil, nil}, nil} = Heap, Cur) ->
+	case {Cur(Head), Cur(Left)} of
+		{V, Vl} when V < Vl -> Heap;
+		_ -> {Left, {Head, nil, nil}, nil}
 	end;
-fix({P, {Pl, Ll, Rl} = L, {Pr, Lr, Rr} = R} = T, Cur) ->
-	case {Cur(P), Cur(Pl), Cur(Pr)} of
-		{V, Vl, Vr} when V < Vl, V < Vr -> T;
-		{_, Vl, Vr} when Vl < Vr -> {Pl, fix({P, Ll, Rl}, Cur), R};
-		_ -> {Pr, L, fix({P, Lr, Rr}, Cur)}
+fix({Head, {Hl, Ll, Rl} = Left, {Hr, Lr, Rr} = Right} = Heap, Cur) ->
+	case {Cur(Head), Cur(Hl), Cur(Hr)} of
+		{V, Vl, Vr} when V < Vl, V < Vr -> Heap;
+		{_, Vl, Vr} when Vl < Vr -> {Hl, fix({Head, Ll, Rl}, Cur), Right};
+		_ -> {Hr, Left, fix({Head, Lr, Rr}, Cur)}
 	end.
