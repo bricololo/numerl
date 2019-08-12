efc7c3a7afeda53525a4442d894ccb0c7165dae8
Laurent Picouleau <l.picouleau@gmail.com>
2019-08-12 22:32:00 +0200

    adding the squarefree backend to sieve

---
+++ src/squarefree.erl
@@ -0,0 +1,31 @@
+-module(squarefree).
+
+-export([init_pq/1, init_pq/2, candidate/1, result/1, next_candidate/1,
+	new_acc/2]).
+
+init_pq(Lim) ->
+	Primes = sieve:list(prime, numerl:isqrt(Lim)),
+	init(pq:new(fun cur/1, fun next/1), Primes, 2).
+
+init_pq(From, To) ->
+	Primes = sieve:list(prime, numerl:isqrt(To)),
+	init(pq:new(fun cur/1, fun next/1), Primes, From).
+
+candidate(V) -> V.
+
+result(L) when is_list(L) -> lists:reverse(L).
+
+next_candidate(N) -> N + 1.
+
+new_acc(Acc, V) when is_list(Acc) -> [V | Acc].
+
+%
+% internals
+%
+
+cur({V, _}) -> V.
+
+next({V, Inc}) -> {V + Inc, Inc}.
+
+init(Bad, [H | T], B) -> init(pq:add(Bad, {H * H, H * H}), T, B);
+init(Bad, [], B) -> {Bad, B, []}.
