6d9849ad9a746c8a352ec8e89a5118c8ef9dded5
Laurent Picouleau <l.picouleau@gmail.com>
2019-08-16 22:27:47 +0200

    slightly faster init

--- src/wheel.erl
+++ src/wheel.erl
@@ -3,10 +3,24 @@
 -export([init/1, next/1]).
 
 
+init([3, 5 | [7 | Divs] = T]) ->
+	Length = lists:foldl(fun(E, L) -> E * L end, 210, Divs),
+	Notches =
+		lists:merge([
+			[Y || Y <- lists:seq(11, 10 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(13, 12 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(17, 16 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(19, 18 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(23, 22 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(29, 28 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(31, 30 + Length, 30), check(T, Y) =/= 0],
+			[Y || Y <- lists:seq(37, 36 + Length, 30), check(T, Y) =/= 0]]),
+	{delta(Notches ++ [hd(Notches) + Length]), []};
 init(Divs) ->
 	Count = lists:foldl(fun(E, P) -> (E - 1) * P end, 1, Divs),
 	Seed = seed(Divs, Count + 1),
 	{delta(Seed), []}.
+
 
 next({[H | T], Acc}) -> {H, {T, [H | Acc]}};
 next({[], Acc}) -> next({lists:reverse(Acc), []}).
