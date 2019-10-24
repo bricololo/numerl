42749039bfa5b455bf881be4b7d67c261e2160eb
Laurent Picouleau <l.picouleau@gmail.com>
2019-10-24 22:08:09 +0200

    more meaningful function names

--- src/sieve.erl
+++ src/sieve.erl
@@ -1,42 +1,44 @@
 -module(sieve).
 
 -export([
-	list/2, list/3, list/4, list/5,
+	up_to/2, from_to/3,
+	b_up_to/3, b_from_to/4,
+	b1b2_from_to/5,
 	ets/3, ets/4, %ets/5, ets/6,
 	foldl/4, foldl/5
 	]).
 
 -export([fast_bump/3]).
 
-list(prime, Lim) ->
+up_to(prime, Lim) ->
 	{Comp, Start, Acc} = prime:init_pq(Lim, [7, 5, 3, 2]),
 	sieve(prime, Comp, Start, Lim, Acc);
-list(squarefree, Lim) ->
+up_to(squarefree, Lim) ->
 	{Bad, Start, Acc} = squarefree:init_pq(Lim),
 	sieve(squarefree, Bad, Start, Lim, Acc).
 
-list(prime, From, To) ->
+from_to(prime, From, To) ->
 	{Temp, Start, Acc} = prime:init_pq(To, [7, 5, 3, 2]),
 	Start_from = prime:start_from(Start, From),
 	Comp = fast_bump(prime, Temp, element(1, Start_from)),
 	sieve(prime, Comp, Start_from, To,
-		lists:takewhile(fun(X) -> X >= From end, Acc));
-list(smooth, B, Lim) ->
+		lists:takewhile(fun(X) -> X >= From end, Acc)).
+
+b_up_to(smooth, B, Lim) ->
 	{Bad, Start, Acc} = smooth:init_pq(B, Lim),
 	sieve(smooth, Bad, Start, Lim, Acc);
-list(power_smooth, B, Lim) -> list(power_smooth, B, B, Lim).
+b_up_to(power_smooth, B, Lim) -> b_from_to(power_smooth, B, B, Lim).
 
-list(smooth, B, From, To) ->
+b_from_to(smooth, B, From, To) ->
 	{Tmp, Start, Acc} = smooth:init_pq(B, {From, To}),
 	Bad = fast_bump(smooth, Tmp, From),
 	sieve(smooth, Bad, Start, To, Acc);
-list(power_smooth, B1, B2, Lim) ->
+b_from_to(power_smooth, B1, B2, Lim) ->
 	{Bad, Start, Acc} = power_smooth:init_pq({B1, B2}, Lim),
 	sieve(power_smooth, Bad, Start, Lim, Acc).
 
 
-%list(smooth, B, B2, From, To) ->
-list(power_smooth, B1, B2, From, To) ->
+b1b2_from_to(power_smooth, B1, B2, From, To) ->
 	{Tmp, _, Acc} = power_smooth:init_pq({B1, B2}, To),
 	Bad = fast_bump(power_smooth, Tmp, From),
 	sieve(power_smooth, Bad, From, To, Acc).
@@ -53,23 +55,11 @@ ets(prime, From, To, Tid) ->
 	[ets:ins(Acc, {P, y}) || P <- [2, 3, 5, 7], P =< From],
 	sieve(prime, Comp, Start_from, To,
 		lists:dropwhile(fun(X) -> X < From end, Acc)).
-%ets(smooth, B, Lim, Tid) -> ets(smooth, B, B, Lim);
-%ets(power_smooth, B, Lim, Tid) -> ets(power_smooth, B, B, Lim);
-
-%ets(smooth, B, B2, Lim, Tid) ->
-%ets(power_smooth, B, B2, Lim, Tid) ->
-
-%ets(smooth, B, B2, From, To, Tid) ->
-%ets(power_smooth, B, B2, From, To, Tid) ->
 
 foldl(prime, Lim, Fun, Acc) -> foldl(prime, 1, Lim, Fun, Acc).
-%foldl(smooth, Lim, Fun, Acc) ->
-%foldl(power_smooth, Lim, Fun, Acc) ->
 foldl(prime, From, To, Fun, Acc) ->
 	{Comp, Start, N_Acc} = prime:init_pq(To, {Fun, Acc, From}),
 	sieve(prime, Comp, Start, To, N_Acc).
-%foldl(smooth, From, To, Fun, Acc) ->
-%foldl(power_smooth, From, To, Fun, Acc) ->
 
 sieve(Module, Bad, V, Lim, Acc) ->
 	Candidate = Module:candidate(V),
